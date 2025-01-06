286e640587c0336427c50731eca982af02b0ca2f
therohitmalakar <rohitmalakar437@gmail.com>
2025-01-06 20:04:29 +0530

    modified index.html

--- js/script.js
+++ js/script.js
@@ -7,3 +7,15 @@ AOS.init({
     offset: 100,
 });
 
+// bmi calculator
+
+document.querySelector("#show-calc").addEventListener("click", function(){
+    document.querySelector(".popup").classList.add("active");
+});
+
+
+document.querySelector(".popup .close-btn").addEventListener("click", function(){
+    document.querySelector(".popup").classList.remove("active");
+});
+
+
